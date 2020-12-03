75977278e1bb4884d8051953f873a9816c610318
Erik Huang <erikhuangdrums@gmail.com>
2020-12-03 19:51:26 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,23 @@
-def using_include(array, element)
+def using_include(arr, ele)
+  return arr.include?(ele)
+end
+
+def using_sort(arr)
 
 end
 
-def using_sort(array)
+def using_reverse(arr)
 
 end
 
-def using_reverse(array)
+def using_first(arr)
 
 end
 
-def using_first(array)
+def using_last(arr)
 
 end
 
-def using_last(array)
-
-end
-
-def using_size(array)
+def using_size(arr)
 
 end
